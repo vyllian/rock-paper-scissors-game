8370173fc6d9825241f3b9ec6de666c4c5f0bcdb
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2025-04-13 13:52:55 +0300

    added "rules" button functionality

---
+++ scripts/rules.js
@@ -0,0 +1,52 @@
+
+const rulesBtn = document.querySelector('.rules-btn')
+
+rulesBtn.addEventListener('click', ()=>{
+    const article = document.querySelector('article')
+    if (article){
+        article.style = 'dislay: block'
+    }else{
+        document.body.appendChild(createRulesArticle())
+    }  
+    const closeBtn = document.getElementById('close-btn')
+    closeBtn.addEventListener('click', ()=>{
+    const article = document.querySelector('article')
+    article.style = 'display: none'
+})
+
+})
+
+const createRulesArticle = ()=>{
+    const h2 = document.createElement('h2')
+    h2.textContent = 'Rules'
+    h2.classList.add('bold')
+
+    const btn = document.createElement('button')
+    btn.type = 'button'
+    btn.id = 'close-btn'
+
+    const closeImg = document.createElement('img')
+    closeImg.src = '../images/icon-close.svg'
+    closeImg.alt = 'close'
+    closeImg.width = '20'
+    btn.appendChild(closeImg)
+
+    const innerDiv = document.createElement('div')
+    innerDiv.append(h2, btn)
+    innerDiv.classList.add('flex')
+
+    const rulesImg = document.createElement('img')
+    rulesImg.src = '../images/image-rules.svg'
+    rulesImg.alt = 'Rules: paper beats rock, rock beats scissors, scissors beat paper'
+    rulesImg.width = '305'
+
+    const outerDiv = document.createElement('div')
+    outerDiv.append(innerDiv, rulesImg)
+    outerDiv.classList.add('rules', 'flex')
+
+   const article = document.createElement('article')
+   article.appendChild(outerDiv)
+   article.classList.add('backdrop')
+   return article
+}
+
